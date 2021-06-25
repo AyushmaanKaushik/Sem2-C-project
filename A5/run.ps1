41b5b7cf87325ec9b43bdb610a98582196bdf5cd
CoderCatA5 <76586285+CoderCatA5@users.noreply.github.com>
2021-06-25 23:28:29 +0530

    Add files via upload

--- A5/run.ps1
+++ A5/run.ps1
@@ -1,1 +1,1 @@
-gcc main.c -o game.exe  -Wall -std=c99 -Wno-missing-braces -I .\include\ -L .\lib\ -lraylib -lopengl32 -lgdi32 -lwinmm 
+gcc main.c essentials.c -o game.exe  -Wall -std=c99 -Wno-missing-braces -I .\include\ -L .\lib\ -lraylib -lopengl32 -lgdi32 -lwinmm 
